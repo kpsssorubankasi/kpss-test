4f7419ac2c05dad12bb4aa425eb91d006748de96
kpsssorubankasi <leventistemi@hotmail.com>
2026-03-27 16:38:35 +0300

    Add files via upload

---
+++ kpss-data.js
@@ -0,0 +1,139 @@
+const kpssData = {
+
+    matematik: [
+        {
+            soru: "5 + 3 kaçtır?",
+            siklar: ["6", "7", "8", "9"],
+            dogru: 2
+        },
+        {
+            soru: "10 - 4 kaçtır?",
+            siklar: ["5", "6", "7", "8"],
+            dogru: 1
+        },
+        {
+            soru: "3 x 4 kaçtır?",
+            siklar: ["7", "12", "9", "10"],
+            dogru: 1
+        }
+    ],
+
+    turkce: [
+        {
+            soru: "Hangisi isimdir?",
+            siklar: ["Koşmak", "Kitap", "Güzel", "Yazmak"],
+            dogru: 1
+        },
+        {
+            soru: "Aşağıdakilerden hangisi fiildir?",
+            siklar: ["Ev", "Gitmek", "Masa", "Kalem"],
+            dogru: 1
+        },
+        {
+            soru: "“Güzel” kelimesi hangi türdür?",
+            siklar: ["Fiil", "Zamir", "Sıfat", "Bağlaç"],
+            dogru: 2
+        }
+    ],
+
+    tarih: [
+        {
+            soru: "İstanbul ne zaman fethedildi?",
+            siklar: ["1453", "1923", "1071", "1517"],
+            dogru: 0
+        },
+        {
+            soru: "Türkiye Cumhuriyeti ne zaman kuruldu?",
+            siklar: ["1920", "1921", "1923", "1925"],
+            dogru: 2
+        },
+        {
+            soru: "Malazgirt Savaşı hangi yılda oldu?",
+            siklar: ["1071", "1453", "1517", "1923"],
+            dogru: 0
+        }
+    ],
+
+    cografya: [
+        {
+            soru: "Türkiye’nin başkenti neresidir?",
+            siklar: ["İstanbul", "Ankara", "İzmir", "Bursa"],
+            dogru: 1
+        },
+        {
+            soru: "Dünyanın en büyük okyanusu hangisidir?",
+            siklar: ["Atlas", "Hint", "Pasifik", "Arktik"],
+            dogru: 2
+        },
+        {
+            soru: "Türkiye hangi kıtadadır?",
+            siklar: ["Avrupa", "Asya", "Afrika", "Asya ve Avrupa"],
+            dogru: 3
+        }
+    ],
+
+    vatandaslik: [
+        {
+            soru: "Türkiye bir hukuk devleti midir?",
+            siklar: ["Evet", "Hayır"],
+            dogru: 0
+        },
+        {
+            soru: "Yasama yetkisi kime aittir?",
+            siklar: ["Cumhurbaşkanı", "TBMM", "Bakanlar Kurulu", "Yargı"],
+            dogru: 1
+        },
+        {
+            soru: "Anayasa nedir?",
+            siklar: [
+                "En üst hukuk kuralları",
+                "Kanun kitabı",
+                "Mahkeme kararı",
+                "Yönetmelik"
+            ],
+            dogru: 0
+        }
+    ],
+
+    guncel: [
+        {
+            soru: "Türkiye’nin para birimi nedir?",
+            siklar: ["Euro", "Dolar", "Türk Lirası", "Sterlin"],
+            dogru: 2
+        },
+        {
+            soru: "2023 yılında Türkiye kaç yaşına girdi?",
+            siklar: ["90", "95", "100", "105"],
+            dogru: 2
+        },
+        {
+            soru: "Türkiye’nin resmi dili nedir?",
+            siklar: ["İngilizce", "Türkçe", "Arapça", "Fransızca"],
+            dogru: 1
+        }
+    ],
+
+    geometri: [
+        {
+            soru: "Üçgenin iç açıları toplamı kaçtır?",
+            siklar: ["90", "180", "270", "360"],
+            dogru: 1
+        },
+        {
+            soru: "Karenin kaç kenarı vardır?",
+            siklar: ["3", "4", "5", "6"],
+            dogru: 1
+        },
+        {
+            soru: "Dikdörtgende karşı kenarlar nasıldır?",
+            siklar: [
+                "Eşit",
+                "Farklı",
+                "Paralel değil",
+                "Kısa"
+            ],
+            dogru: 0
+        }
+    ]
+
+};
